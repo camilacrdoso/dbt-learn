b2b6cb6bc357aba8c392e35c72e228de7756b81e
Camila Cardoso <camila.cardoso.lit@gmail.com>
2025-01-20 18:14:42 +0000

    refactors stg_stripe__payments to convert amount from cents to dollars

--- models/stg/stripe/stg_stripe__payments.sql
+++ models/stg/stripe/stg_stripe__payments.sql
@@ -3,7 +3,7 @@ SELECT
     orderid AS order_id,
     paymentmethod AS payment_method,
     status,
-    amount,
+    amount / 100 AS amount,
     created AS created_at,
     _batched_at AS batched_at
 FROM `dbt-tutorial`.stripe.payment
