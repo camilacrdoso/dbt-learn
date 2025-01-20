0f7bd3c2905c14e82969b89f35200b91b1ac4d5a
Camila Cardoso <camila.cardoso.lit@gmail.com>
2025-01-20 18:18:47 +0000

    refactors fct_orders to aggregate order amount

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,6 +1,14 @@
+WITH grouped_payments AS (
+    SELECT
+        order_id,
+        SUM(CASE WHEN status = 'success' THEN amount ELSE NULL END) AS amount,
+    FROM {{ ref('stg_stripe__payments') }}
+    GROUP BY order_id
+)
+
 SELECT 
     order_id,
     customer_id,
     amount
 FROM {{ ref('stg_jaffle_shop__orders') }}
-INNER JOIN {{ ref('stg_stripe__payments') }} USING(order_id)
+INNER JOIN grouped_payments USING(order_id)
